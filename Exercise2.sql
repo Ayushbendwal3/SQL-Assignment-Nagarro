4267a248de975fa83acfd3349e81510abb84795b
sanyam96 <sanyam.bvcoe96@gmail.com>
2018-08-12 17:32:56 +0530

    SQL Assignments

---
+++ Exercise2.sql
@@ -0,0 +1,41 @@
+/*
+Write separate queries using 
+a join, 
+a subquery, 
+a CTE, and 
+then an EXISTS to list all AdventureWorks customers who have not placed an order.
+*/
+
+--CONNECT TO DATABASE
+USE AdventureWorks2012;
+
+--JOINS
+SELECT c.CustomerID
+FROM Sales.SalesOrderHeader soh
+	RIGHT JOIN Sales.Customer c ON soh.CustomerID = c.CustomerID
+WHERE SalesOrderID IS NULL;
+
+
+--SUB QUERY
+SELECT c.CustomerID
+FROM Sales.Customer c
+WHERE c.CustomerID NOT IN (SELECT CustomerID FROM Sales.SalesOrderHeader);
+
+
+--CTE
+WITH CustomersWithoutOrder (CustmerID) 
+AS 
+(
+	SELECT c.CustomerID
+	FROM Sales.SalesOrderHeader soh
+	RIGHT JOIN Sales.Customer c ON soh.CustomerID = c.CustomerID
+	WHERE SalesOrderID IS NULL
+)
+SELECT * FROM CustomersWithoutOrder;
+	
+--EXIST
+SELECT c.CustomerID
+FROM Sales.Customer c
+WHERE NOT EXISTS (SELECT s.CustomerID 
+				FROM Sales.SalesOrderHeader AS s
+				WHERE s.CustomerID = c.CustomerID);
